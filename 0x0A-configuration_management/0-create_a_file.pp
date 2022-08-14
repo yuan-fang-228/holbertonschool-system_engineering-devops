72bf90e217f41a0bcf97e07fd290eca032c839fe
yuan-fang-228 <fy0228@gmail.com>
2022-08-14 06:42:58 -0700

    task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#using Puppet to create a file in /tmp
+file { 'school':
+  ensure  => present,
+  path    => '/tmp/school'
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet'
+}
