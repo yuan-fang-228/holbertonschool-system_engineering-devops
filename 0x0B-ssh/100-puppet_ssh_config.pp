a461bb055270e292a0bc46270dc1c9d2a7a8d221
yuan-fang-228 <fy0228@gmail.com>
2022-08-14 07:11:35 -0700

    change backd to my code

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 # using puppet update configuration file
-file_line {'pwd no':
+file_line {'Turn off passwd auth':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
 }
 
-file_line { 'auth':
+file_line { 'Declare identity file':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school',
