35648fd0e9d84e1a72e7d25ca120ebbe1cfdbe21
yuan-fang-228 <fy0228@gmail.com>
2022-08-14 06:59:42 -0700

    task 4

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# using puppet update configuration file
+file_line {'pwd no':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/holberton',
+}
