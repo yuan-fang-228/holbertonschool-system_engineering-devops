511124a6d6e6e40347d66a55f5d85d76709e9fe7
yuan-fang-228 <fy0228@gmail.com>
2022-09-12 04:28:17 -0700

    alternative

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
-"""Export data of all employees in JSON format"""
+"""Export data in JSON format"""
 import json
 import requests
 import sys
 import urllib
 
 
+def singleRecordJson(url, userId):
+    """export single record to json format"""
+    user = requests.get(url + "/users/{}".format(userId)).json()
+    todos = requests.get(url + "/users/{}/todos/".format(userId)).json()
+    username = user.get("username")
+    tasks = []
+    for task in todos:
+        task_dic = {}
+        task_dic["task"] = task.get("title")
+        task_dic["completed"] = task.get("completed")
+        task_dic["username"] = username
+        tasks.append(task_dic)
+    return tasks
+
+
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com"
     users = requests.get(url + "/users/").json()
     dic = {}
+    for user in users:
+        dic[user.get("id")] = singleRecordJson(url, user.get("id"))
     with open("todo_all_employees.json", "w") as alljsonfile:
-        for user in users:
-            userId = user.get("id")
-            todo = requests.get(url + "/users/{}/todos/".format(userId)).json()
-            username = user.get("username")
-            tasks = []
-            for task in todo:
-                task_dic = {}
-                task_dic["task"] = task.get("title")
-                task_dic["completed"] = task.get("completed")
-                task_dic["username"] = username
-                tasks.append(task_dic)
-            dic[userId] = tasks
         json.dump(dic, alljsonfile)
