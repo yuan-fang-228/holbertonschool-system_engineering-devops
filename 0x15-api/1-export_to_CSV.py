f04a8dbb5e0b46ae6fb6b66749fa19718cc79160
yuan-fang-228 <fy0228@gmail.com>
2022-09-11 23:14:40 -0700

    change to username

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """Export data in CSV format"""
-import urllib
+import csv
 import requests
 import sys
-import csv
+import urllib
+
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com"
     user = requests.get(url + "/users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "/users/{}/todos/".format(sys.argv[1])).json()
-    username = user.get("name")
+    username = user.get("username")
     with open("{}.csv".format(sys.argv[1]), "w", encoding="UTF8",
               newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
